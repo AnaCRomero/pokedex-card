585332a5fe9711efa48cb168c8ee87ec2a0d942d
Ana Cecilia Romero Fortin <ana.romero@tengo.hn>
2026-02-08 18:59:49 -0600

    refactor: improve navigation, rendering and validation
    - Moved main card logic into PokemonCard module
    - Refactored navigation logic to be DRY
    - Added DOM caching and validation
    - Applied loop-based rendering for stats
    - Replaced switch statement with type-color map

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import PokemonCard from "./modules/PokemonCards.js";
+
 const pokemons = [
   {
     id: 1,
@@ -31,69 +33,8 @@ const pokemons = [
   },
 ];
 
-let currentIndex = 0;
+const card = new PokemonCard(pokemons);
 
 document.addEventListener("DOMContentLoaded", function () {
-  updateCard();
+  card.init();
 });
-
-function getElement(id) {
-  return document.getElementById(id);
-}
-
-function updateCard() {
-  let pokemonName = getElement("pokemon-name");
-  let pokemonId = getElement("pokemon-id");
-  let pokemonType = getElement("pokemon-type");
-  let pokemonHp = getElement("pokemon-hp");
-  let pokemonAttack = getElement("pokemon-attack");
-  let pokemonDefense = getElement("pokemon-defense");
-  let pokemonImage = getElement("pokemon-image");
-  pokemonName.textContent = pokemons[currentIndex].name;
-  pokemonId.textContent = `#${pokemons[currentIndex].id.toString()}`;
-  pokemonType.textContent = pokemons[currentIndex].type;
-  pokemonHp.textContent = pokemons[currentIndex].hp;
-  pokemonAttack.textContent = pokemons[currentIndex].attack;
-  pokemonDefense.textContent = pokemons[currentIndex].defense;
-  pokemonImage.src = pokemons[currentIndex].image;
-
-  updateCardColor(pokemons[currentIndex].type);
-}
-
-function nextPokemon() {
-  currentIndex++;
-
-  if (currentIndex >= pokemons.length) {
-    currentIndex = 0;
-  }
-
-  updateCard();
-}
-
-function prevPokemon() {
-  currentIndex--;
-
-  if (currentIndex < 0) {
-    currentIndex = pokemons.length - 1;
-  }
-
-  updateCard();
-}
-
-function updateCardColor(type) {
-  const card = getElement("pokemon-type");
-
-  switch (type) {
-    case "Fuego":
-      card.style.backgroundColor = "#fb6c6c";
-      break;
-    case "Agua":
-      card.style.backgroundColor = "#76bdfe";
-      break;
-    case "Planta":
-      card.style.backgroundColor = "#48d0b0";
-      break;
-    default:
-      card.style.backgroundColor = "#f2f2f2";
-  }
-}
